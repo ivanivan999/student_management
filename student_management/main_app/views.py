ebec7a99a370046d5c954d0dabae876b50bd4f8e
ivan <myu058@uottawa.ca>
2024-10-02 19:57:33 -0400

    Implement pagination on the student list page

--- student_management/main_app/views.py
+++ student_management/main_app/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Student
 from .forms import StudentForm
 
@@ -10,12 +9,31 @@ class StudentListView(LoginRequiredMixin, ListView):
     model = Student
     template_name = 'main_app/student_list.html'
     context_object_name = 'students'
+    paginate_by = 10  # Number of students per page
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         if query:
-            return Student.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))
-        return Student.objects.all()
+            return Student.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query)).order_by('first_name')
+        return Student.objects.all().order_by('first_name')
+    
+    def get_paginate_by(self, queryset):
+        paginate_by = self.request.GET.get('paginate_by', 10)
+        try:
+            paginate_by = int(paginate_by)
+            if paginate_by < 1:
+                paginate_by = 10
+        except ValueError:
+            paginate_by = 10
+        return paginate_by
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+        context['start_index'] = page_obj.start_index()
+        context['end_index'] = page_obj.end_index()
+        return context
 
 class StudentDetailView(LoginRequiredMixin, DetailView):
     model = Student
