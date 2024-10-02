52dff061de2f54d36e76d85a8ecd6d5eab8e0ddd
ivan <myu058@uottawa.ca>
2024-10-02 15:56:33 -0400

    Add search functionality to student list page

--- student_management/main_app/urls.py
+++ student_management/main_app/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', views.StudentListView.as_view(), name='student_list'),
     path('add/', views.StudentCreateView.as_view(), name='student_add'),
     path('<int:pk>/edit/', views.StudentUpdateView.as_view(), name='student_edit'),
-    path('<int:pk>/delete/', views.StudentDeleteView.as_view(), name='student_delete'),
     path('<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
 ]
--- student_management/main_app/views.py
+++ student_management/main_app/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from .models import Student
 from .forms import StudentForm
 
 class StudentListView(LoginRequiredMixin, ListView):
     model = Student
-    template_name = 'student_list.html'
+    template_name = 'main_app/student_list.html'
     context_object_name = 'students'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Student.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))
+        return Student.objects.all()
 
 class StudentDetailView(LoginRequiredMixin, DetailView):
     model = Student
@@ -25,9 +32,4 @@ class StudentUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
     form_class = StudentForm
     template_name = 'student_form.html'
-    success_url = reverse_lazy('student_list')
-
-class StudentDeleteView(LoginRequiredMixin, DeleteView):
-    model = Student
-    template_name = 'student_confirm_delete.html'
     success_url = reverse_lazy('student_list')
